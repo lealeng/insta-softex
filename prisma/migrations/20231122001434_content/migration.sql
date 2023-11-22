7c3952721e4bb19ed998f39af6a16aa7de63e718
Daniel Leal <daniel.suportte@gmail.com>
2023-11-21 21:21:24 -0300

    content field

---
+++ prisma/migrations/20231122001434_content/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Likes" (
+    "id" SERIAL NOT NULL,
+    "liked" BOOLEAN NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "postId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Likes_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Likes" ADD CONSTRAINT "Likes_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Likes" ADD CONSTRAINT "Likes_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
